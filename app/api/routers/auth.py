0cbe4eacdd24df15f9cad222398a000e5be76828
Chidera Mosanya <deramosanya@gmail.com>
2025-03-21 19:36:36 -0400

    implemented jwt encode

--- app/api/routers/auth.py
+++ app/api/routers/auth.py
@@ -4,6 +4,7 @@ from app.db import database
 from app.db import models
 from app.utils import security
 from sqlalchemy.orm import Session
+from app.utils import security
 
 router = APIRouter(
     prefix="/auth",
@@ -23,4 +24,8 @@ def login(user_credentials: UserLogin, db: Session = Depends(database.get_db)):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail="Invalid credentials")
 
-    return {"token": "example_token"}
+    access_token = security.create_access_token(
+        data={"user_id": user.id}
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
