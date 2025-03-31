ff4d5777f3cb1b715a8061ebfaba6bff5b1d96e8
Chidera Mosanya <deramosanya@gmail.com>
2025-03-31 11:16:02 -0400

    changes to auth and security

--- app/api/routers/auth.py
+++ app/api/routers/auth.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter,status, Depends, HTTPException
+from fastapi import APIRouter, status, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from app.api.schemas import UserLogin
 from app.db import database
 from app.db import models
+from app.api.schemas import Token
 from app.utils import security
 from sqlalchemy.orm import Session
 from app.utils import security
@@ -12,9 +14,9 @@ router = APIRouter(
 )
 
 
-@router.post("/login")
-def login(user_credentials: UserLogin, db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+@router.post("/login", response_model=Token)
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
--- app/utils/security.py
+++ app/utils/security.py
@@ -1,9 +1,13 @@
 import os
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
+from app.api import schemas
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import status, HTTPException, Depends
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
+oauth_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 JWT_SECRET = os.getenv("JWT_SECRET")
 ALGORITHM = "HS256"
@@ -31,3 +35,26 @@ def create_access_token(data: dict):
     )
 
     return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        decoded_token = jwt.decode(token, JWT_SECRET, ALGORITHM)
+        _id: str = decoded_token.get("user_id")
+
+        if _id is None:
+            raise credentials_exception
+
+        token_data = schemas.TokenData(id=_id)
+    except JWTError:
+        raise credentials_exception
+
+
+def get_current_user(token: str = Depends(oauth_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+
+    verify_access_token(token, credentials_exception)
